7ed463a4d3cba9c91465ac8752037cd0ca76cd9d
Michael Noronha <michaeltnoronha@gmail.com>
2019-12-15 20:36:32 -0800

    day 2

---
+++ day02/part1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+with open("input.txt") as f:
+    memory = list(map(int, f.read().split(',')))
+
+memory[1] = 12
+memory[2] = 2
+
+pos = 0
+while memory[pos] != 99:
+    if memory[pos] == 1:
+        memory[memory[pos+3]] = memory[memory[pos+1]] + memory[memory[pos+2]]
+    elif memory[pos] == 2:
+        memory[memory[pos+3]] = memory[memory[pos+1]] * memory[memory[pos+2]]
+    pos += 4
+
+print(memory[0])
---
+++ day02/part2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import copy
+
+with open("input.txt") as f:
+    memory = list(map(int, f.read().split(',')))
+
+for noun in range(100):
+    for verb in range(100):
+        mem = copy.deepcopy(memory)
+        mem[1] = noun
+        mem[2] = verb
+        pos = 0
+        while mem[pos] != 99:
+            if mem[pos] == 1:
+                mem[mem[pos+3]] = mem[memory[pos+1]] + mem[mem[pos+2]]
+            elif memory[pos] == 2:
+                mem[mem[pos+3]] = mem[memory[pos+1]] * mem[mem[pos+2]]
+            pos += 4
+
+        if mem[0] == 19690720:
+            print(100 * noun + verb)
