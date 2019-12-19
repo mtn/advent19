9ad01f32e42d041173f3ab93662aa689b72b9dd4
Michael Noronha <michaeltnoronha@gmail.com>
2019-12-18 19:08:51 -0800

    day 8

---
+++ day08/part1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+with open("input.txt") as f:
+  inp = f.read()
+
+layer_len = 150
+num_layers = len(inp) / 150
+
+min_zeros = None
+least_zeros_layer = None
+for i in range(num_layers):
+  layer = inp[i*layer_len: (i+1)*layer_len]
+  num_zeros = layer.count("0")
+
+  if min_zeros is None or num_zeros < min_zeros:
+    min_zeros = num_zeros
+    least_zeros_layer = i
+
+least_zeros_layer = inp[least_zeros_layer*layer_len: (least_zeros_layer+1)*layer_len]
+print(least_zeros_layer.count("1") * least_zeros_layer.count("2"))
---
+++ day08/part2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+with open("input.txt") as f:
+  inp = f.read()
+
+layer_len = 150
+num_layers = len(inp) / 150
+
+BLACK = str(0)
+WHITE = str(1)
+TRANSPARENT = str(2)
+
+top_pixels = [None] * layer_len
+for i in range(num_layers):
+  layer = inp[i*layer_len: (i+1)*layer_len]
+
+  for i, l in enumerate(layer):
+    if top_pixels[i] is not None:
+      continue
+    if l == WHITE or l == BLACK:
+      top_pixels[i] = l
+
+num_rows = 6
+num_cols = 25
+for row_num in range(num_rows):
+  row = top_pixels[row_num*num_cols: (row_num+1)*num_cols]
+  row = "".join(row).replace("1", "#").replace("0", " ")
+  print(row)
